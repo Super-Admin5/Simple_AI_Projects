12fdcd202db7d2309e0eaa477b68519c9f8ab9f4
Super-Admin5 <krishp728@gmail.com>
2025-01-13 10:50:39 +0530

    Day 01 buildAbrain

---
+++ Vegetable Disease Classification/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,5 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import tensorflow as tf
+print("GPU devices:", tf.config.list_physical_devices('GPU'))
+print("Is CUDA available:", tf.test.is_built_with_cuda())
