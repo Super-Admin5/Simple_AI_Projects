f566a2ee55efb88f3db506a1b225327dfd33f8d6
Super-Admin5 <krishp728@gmail.com>
2025-02-01 16:04:20 +0000

    Reinitialized repository and added changes

--- LearningOpenCV/python-files/read.py
+++ LearningOpenCV/python-files/read.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cv2 as cv
 
 # img = cv.imread('../Resources/Photos/cat_large.jpg')
@@ -15,3 +16,22 @@ while True:
 capture.release()
 cv.destroyAllWindows()
 
+=======
+import cv2 as cv
+
+# img = cv.imread('../Resources/Photos/cat_large.jpg')
+
+# cv.imshow('cat',img)
+
+capture = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('video',frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
+>>>>>>> a6cd6acd (Reinitialized repository and added changes)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # Simple_ML_Projects
+=======
+# Simple_ML_Projects
+>>>>>>> a6cd6acd (Reinitialized repository and added changes)
